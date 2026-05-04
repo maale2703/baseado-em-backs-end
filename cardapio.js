7310ea2c568de373957f1c2151ef1a380a25dc5c
maryana vitoria <160244232+maale2703@users.noreply.github.com>
2026-05-04 11:51:07 +0000

    versao1

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,14 @@
+const cardapio = [
+    {nome: "X-Burguer", preco: 18.90, categoria: "lanche"},
+    {nome: "Batata-Frita", preco: 9.50, categoria: "acompanhamento"},
+    {nome: "Suco de Laranja", preco: 7.00, categoria: "bebida"},
+    {nome: "Milkshake", preco: 12.00, categoria: "sobremesa"}
+];
 
+//console.log(cardapio); //Impressão dos valores informados
+
+const pedido = [];
+
+console.log(cardapio[0].nome);
+console.log(cardapio[1].preco); //preço da batata
+console.log(cardapio[2].categoria); //categoria do suco de laranja
