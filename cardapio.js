2cf2649da7b208773b0be873dde1fb55f6fc1732
maryana vitoria <160244232+maale2703@users.noreply.github.com>
2026-05-05 11:52:51 +0000

    versao1

--- cardapio.js
+++ cardapio.js
@@ -7,8 +7,16 @@ const cardapio = [
 
 //console.log(cardapio); //Impressão dos valores informados
 
-const pedido = [];
+const pedido = []; //lista vazia, vai receber os itens escolhidos
 
 console.log(cardapio[0].nome);
 console.log(cardapio[1].preco); //preço da batata
 console.log(cardapio[2].categoria); //categoria do suco de laranja
+
+function exibirCardapio(lista) {
+    console.log("===CARDAPIO===");
+    console.log(`1. ${lista[0].nome} - R$ ${lista[0].preco}`)
+    console.log(`2. ${lista[1].nome} - R$ ${lista[1].preco}`)
+    console.log(`3. ${lista[2].nome} - R$ ${lista[2].preco}`)
+    console.log(`4. ${lista[3].nome} - R$ ${lista[3].preco}`)
+}
